9fd3f200db2dc097b9c3889755da1fde8417869e
Kang Kujun <donato14@naver.com>
2022-03-17 11:03:03 +0900

    🚧 Add write function

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(8888, function () {
+  console.log('listening on 8888')
+});
+//서버를 띄우기 위한 기본 셋팅(express 라이브러리)
+
+app.get('/test', function (req, resp) {
+  resp.send('테스트입니다.');
+});
+
+app.get('/', function (req, resp) {
+  resp.sendFile(__dirname + '/index.html');
+});
+
+app.get('/write', function (req, resp) {
+  resp.sendFile(__dirname + '/write.html');
+});
+
+app.post('/add', function (req, resp) {
+  resp.send('전송완료');
+  console.log(req.body);
+  console.log(req.body.title);
+  console.log(req.body.date);
+});
